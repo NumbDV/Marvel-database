64e3b3af43c7a21c7a9ccc380115832a10420308
Den <d3n.vor@gmail.com>
2022-06-12 12:48:27 +0300

    added ref and focus to selected character

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import propTypes from 'prop-types';
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -127,6 +128,10 @@ const View = ({char}) => {
 			</ul>
 		</>
 	)
+}
+
+CharInfo.propTypes = {
+	charId: propTypes.number
 }
 
 export default CharInfo;
